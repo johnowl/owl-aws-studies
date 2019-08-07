e714e8d9e013b4b69be5e8c2b9b8a940a6cb452a
João Paulo <johnowl@gmail.com>
2019-08-06 22:30:48 -0300

    Call variables.sh and install.sh when creating an EC2 instance.

--- aws/create.sh
+++ aws/create.sh
@@ -1,1 +1,9 @@
+echo "Creating variables"
+source variables.sh
+
+echo "Creating instance"
 aws ec2 run-instances --image-id $AWS_IMAGE_ID --count 1 --instance-type t2.micro --key-name $AWS_KEYPAIR_NAME --security-group-ids $AWS_SECURITY_GROUP_ID --subnet-id $AWS_SUBNET_ID | jq '.Instances[0].InstanceId' | sed 's/"//g' >> ec2_instances
+echo "Instance created $(cat ec2_instances)"
+
+echo "Installing software"
+source install.sh
